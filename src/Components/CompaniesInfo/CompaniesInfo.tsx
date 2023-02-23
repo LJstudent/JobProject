06a171c0c9bdeb39a980d8725857463944e95750
LJstudent <poepjan123_2@hotmail.com>
2023-02-23 02:23:49 +0100

    Add the last info to the card

--- src/Components/CompaniesInfo/CompaniesInfo.tsx
+++ src/Components/CompaniesInfo/CompaniesInfo.tsx
@@ -1,6 +1,6 @@
 import { Box, Flex, Heading, IconButton, ScrollView, Stack, Text } from 'native-base';
 import * as React from 'react';
-import { Feather, FontAwesome, AntDesign, SimpleLineIcons, MaterialIcons } from '@expo/vector-icons';
+import { Feather, FontAwesome, AntDesign, SimpleLineIcons, MaterialIcons, FontAwesome5 } from '@expo/vector-icons';
 
 export default function CompaniesInfo() {
     return (
@@ -75,7 +75,7 @@ export default function CompaniesInfo() {
                                 name: "check",
                                 color: "green.500"
                             }} />
-                            <Text ml="-4" alignSelf="center" fontSize={'md'}>There are Frontend developers in many teams and keeping in sync with this group is important for maintaining our scalability. You will collaborate across streams and time zones to make things happen</Text>
+                            <Text ml="-4" alignSelf="center" fontSize={'md'}>There are Frontend developers in many teams and keeping in sync with this group is important for maintaining our scalability. You will collaborate across streams and time zones to make things happen.</Text>
                         </Stack>
                     </Box>
 
@@ -91,7 +91,7 @@ export default function CompaniesInfo() {
                                     }} />
                                     <Heading alignSelf="center" size={"sm"} color={'violet.500'} fontWeight='medium'>Code</Heading>
                                 </Stack>
-                                <Text fontSize={'md'}>React, SQL, Redux, Typescript, Jest and Enzyme</Text>
+                                <Text mt={1} fontSize={'md'}>React, SQL, Redux, Typescript, Jest and Enzyme</Text>
                             </Stack>
                             <Stack w={'15%'} mr={5}>
                                 <Stack direction={'row'}>
@@ -102,7 +102,7 @@ export default function CompaniesInfo() {
                                     }} />
                                     <Heading alignSelf="center" size={"sm"} color={'violet.500'} fontWeight='medium'>Work experience</Heading>
                                 </Stack>
-                                <Text fontSize={'md'}>0-2 years</Text>
+                                <Text mt={1} fontSize={'md'}>0-2 years</Text>
                             </Stack>
                             <Stack w={'20%'} mr={5}>
                                 <Stack direction={'row'}>
@@ -113,7 +113,7 @@ export default function CompaniesInfo() {
                                     }} />
                                     <Heading alignSelf="center" size={"sm"} color={'violet.500'} fontWeight='medium'>Team effort</Heading>
                                 </Stack>
-                                <Text fontSize={'md'}>You are interested in growing the team with us and mentor new joiners</Text>
+                                <Text mt={1} fontSize={'md'}>You are interested in growing the team with us and mentor new joiners</Text>
                             </Stack>
                             <Stack w={'20%'} mr={5}>
                                 <Stack direction={'row'}>
@@ -124,7 +124,7 @@ export default function CompaniesInfo() {
                                     }} />
                                     <Heading alignSelf="center" size={"sm"} color={'violet.500'} fontWeight='medium'>Target</Heading>
                                 </Stack>
-                                <Text fontSize={'md'}>You always trying to make it work while keeping the bigger picture in mind.</Text>
+                                <Text mt={1} fontSize={'md'}>You always trying to make it work while keeping the bigger picture in mind.</Text>
                             </Stack>
                             <Stack w={'20%'} mr={5}>
                                 <Stack direction={'row'}>
@@ -135,7 +135,7 @@ export default function CompaniesInfo() {
                                     }} />
                                     <Heading alignSelf="center" size={"sm"} color={'violet.500'} fontWeight='medium'>Like</Heading>
                                 </Stack>
-                                <Text fontSize={'md'}>You actively share your knowledge and enthusiasm to develop yourself and your team.</Text>
+                                <Text mt={1} fontSize={'md'}>You actively share your knowledge and enthusiasm to develop yourself and your team.</Text>
                             </Stack>
                             <Stack w={'20%'} mr={5}>
                                 <Stack direction={'row'}>
@@ -146,57 +146,104 @@ export default function CompaniesInfo() {
                                     }} />
                                     <Heading alignSelf="center" size={"sm"} color={'violet.500'} fontWeight='medium'>Graduation</Heading>
                                 </Stack>
-                                <Text fontSize={'md'}>You have a BSc or MSc in Computer Science</Text>
+                                <Text mt={1} fontSize={'md'}>You have a BSc or MSc in Computer Science</Text>
                             </Stack>
                         </Flex>
                     </Box>
 
                     <Box>
+                        <Stack mt={10} direction="row">
+                            <IconButton disabled variant="ghost" width={20} ml="-8" _icon={{
+                                as: FontAwesome5,
+                                name: "building",
+                                color: "black"
+                            }} />
+                            <Heading ml="-4" alignSelf="center" size={'sm'}>What we offer</Heading>
+                        </Stack>
+                        <Flex mt={2} direction={'row'} flexWrap={"wrap"}>
+                            <Stack w={'20%'} mr={5}>
+                                <Stack direction={'row'}>
+                                    <IconButton disabled variant="ghost" justifyContent={'flex-start'} pl={0} _icon={{
+                                        as: FontAwesome,
+                                        name: "euro",
+                                        color: "violet.500"
+                                    }} />
+                                    <Heading alignSelf="center" size={"sm"} color={'violet.500'} fontWeight='medium'>Salary</Heading>
+                                </Stack>
+                                <Text mt={1} fontSize={'md'}>10000-20000 euro each month</Text>
+                            </Stack>
+                            <Stack w={'20%'} mr={5}>
+                                <Stack direction={'row'}>
+                                    <IconButton disabled variant="ghost" justifyContent={'flex-start'} pl={0} _icon={{
+                                        as: Feather,
+                                        name: "users",
+                                        color: "violet.500"
+                                    }} />
+                                    <Heading alignSelf="center" size={"sm"} color={'violet.500'} fontWeight='medium'>Team</Heading>
+                                </Stack>
+                                <Text mt={1} fontSize={'md'}>A development department of 250 people working on our product</Text>
+                            </Stack>
+                            <Stack w={'20%'} mr={5}>
+                                <Stack direction={'row'}>
+                                    <IconButton disabled variant="ghost" justifyContent={'flex-start'} pl={0} _icon={{
+                                        as: FontAwesome5,
+                                        name: "building",
+                                        color: "violet.500"
+                                    }} />
+                                    <Heading alignSelf="center" size={"sm"} color={'violet.500'} fontWeight='medium'>Office in Amsterdam</Heading>
+                                </Stack>
+                                <Text mt={1} fontSize={'md'}>A office where you can relax, and we have a flexible work culture so only once in the two weeks we are expecting you at the office</Text>
+                            </Stack>
+                        </Flex>
+
+                    </Box>
+
+                    <Box>
                         <Heading mt={10} size={'sm'}>Extra's</Heading>
                         <Flex direction={'row'} flexWrap={"wrap"}>
-                            <Stack w={'20%'} mr={5} space={4} mt={2} textAlign="center">
-                                    <IconButton disabled mx={'auto'} rounded="lg" backgroundColor="white" variant="solid" shadow={5} _icon={{
-                                        as: AntDesign,
-                                        name: "car",
-                                        color: "violet.400",
-                                        size: 10
-                                    }} />
+                            <Stack w={'15%'} space={4} mt={2} textAlign="center">
+                                <IconButton disabled mx={'auto'} rounded="lg" backgroundColor="white" variant="solid" shadow={5} _icon={{
+                                    as: AntDesign,
+                                    name: "car",
+                                    color: "violet.400",
+                                    size: 8
+                                }} />
                                 <Text fontSize={'md'}>Tesla model 3</Text>
                             </Stack>
-                            <Stack w={'20%'} mr={5} space={4} mt={2} textAlign="center">
-                                    <IconButton disabled mx={'auto'} rounded="lg" backgroundColor="white" variant="solid" shadow={5} _icon={{
-                                        as: MaterialIcons,
-                                        name: "fitness-center",
-                                        color: "violet.400",
-                                        size: 10
-                                    }} />
+                            <Stack w={'15%'} space={4} mt={2} textAlign="center">
+                                <IconButton disabled mx={'auto'} rounded="lg" backgroundColor="white" variant="solid" shadow={5} _icon={{
+                                    as: MaterialIcons,
+                                    name: "fitness-center",
+                                    color: "violet.400",
+                                    size: 8
+                                }} />
                                 <Text fontSize={'md'}>In house Gym</Text>
                             </Stack>
-                            <Stack w={'20%'} mr={5} space={4} mt={2} textAlign="center">
-                                    <IconButton disabled mx={'auto'} rounded="lg" backgroundColor="white" variant="solid" shadow={5} _icon={{
-                                        as: AntDesign,
-                                        name: "areachart",
-                                        color: "violet.400",
-                                        size: 10
-                                    }} />
+                            <Stack w={'15%'} space={4} mt={2} textAlign="center">
+                                <IconButton disabled mx={'auto'} rounded="lg" backgroundColor="white" variant="solid" shadow={5} _icon={{
+                                    as: AntDesign,
+                                    name: "areachart",
+                                    color: "violet.400",
+                                    size: 8
+                                }} />
                                 <Text fontSize={'md'}>Annually growing salary</Text>
                             </Stack>
-                            <Stack w={'20%'} mr={5} space={4} mt={2} textAlign="center">
-                                    <IconButton disabled mx={'auto'} rounded="lg" backgroundColor="white" variant="solid" shadow={5} _icon={{
-                                        as: AntDesign,
-                                        name: "rocket1",
-                                        color: "violet.400",
-                                        size: 10
-                                    }} />
+                            <Stack w={'15%'} space={4} mt={2} textAlign="center">
+                                <IconButton disabled mx={'auto'} rounded="lg" backgroundColor="white" variant="solid" shadow={5} _icon={{
+                                    as: AntDesign,
+                                    name: "rocket1",
+                                    color: "violet.400",
+                                    size: 8
+                                }} />
                                 <Text fontSize={'md'}>Startup</Text>
                             </Stack>
-                            <Stack w={'20%'} mr={5} space={4} mt={2} textAlign="center">
-                                    <IconButton disabled mx={'auto'} rounded="lg" backgroundColor="white" variant="solid" shadow={5} _icon={{
-                                        as: MaterialIcons,
-                                        name: "flight",
-                                        color: "violet.400",
-                                        size: 10
-                                    }} />
+                            <Stack w={'15%'} space={4} mt={2} textAlign="center">
+                                <IconButton disabled mx={'auto'} rounded="lg" backgroundColor="white" variant="solid" shadow={5} _icon={{
+                                    as: MaterialIcons,
+                                    name: "flight",
+                                    color: "violet.400",
+                                    size: 8
+                                }} />
                                 <Text fontSize={'md'}>Possibility to work abroad</Text>
                             </Stack>
                         </Flex>
