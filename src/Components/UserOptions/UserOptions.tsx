88f2f2d1faf08ea1ccfec64489d5cab13f467fe3
LJstudent <poepjan123_2@hotmail.com>
2023-03-18 03:03:33 +0100

    Responsive UserProfile

--- src/Components/UserOptions/UserOptions.tsx
+++ src/Components/UserOptions/UserOptions.tsx
@@ -1,12 +1,11 @@
+import { Feather } from '@expo/vector-icons';
+import { Button, Icon } from 'native-base';
 import React from 'react';
 import { Dimensions, Platform, View } from 'react-native';
-import { Box, Checkbox, Divider, Flex, FormControl, Heading, IconButton, Input, Stack, Text } from 'native-base';
 import Animated, {
     Easing, useAnimatedStyle, useSharedValue, withTiming
 } from 'react-native-reanimated';
-import { Chip } from '@rneui/themed';
-import { Feather } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
+import UserProfile from './UserProfile';
 
 // FIXME need reanimated update, see https://github.com/software-mansion/react-native-reanimated/issues/3355
 if (Platform.OS === 'web') {
@@ -65,158 +64,59 @@ export default function UserOptions() {
         <View
             style={{
                 flexDirection: 'row',
-
             }}>
             <View
                 style={{
                     alignItems: "flex-start",
                     flexDirection: 'column',
-                    marginLeft: '2%',
-                    width: 100
                 }}>
                 <Animated.View
-                    style={[{}, style]}
+                    style={[{ width: '100%' }, style]}
                 >
-                    <Text>Profile</Text>
+                    <Button onPress={() => alert("hello")} mt={2} paddingLeft={5} justifyContent={'flex-start'} borderRadius={'2xl'} variant={'ghost'} mb={2} leftIcon={<Icon as={Feather} size='md' name="user" marginRight={4} />} _text={{
+                        fontSize: {
+                            base: 'none',
+                            sm: 'sm',
+                            md: 'md',
+                            lg: 'lg',
+                            xl: 'lg',
+                        }
+                    }}>
+                        Profile
+                    </Button>
                 </Animated.View>
                 <Animated.View
-                    style={[{}, style1]}
+                    style={[{ width: '100%' }, style1]}
                 >
-                    <Text>Settings</Text>
+                    <Button paddingLeft={5} justifyContent={'flex-start'} borderRadius={'2xl'} variant={'ghost'} mb={2} leftIcon={<Icon as={Feather} size='md' name="settings" marginRight={4} />} _text={{
+                        fontSize: {
+                            base: 'none',
+                            sm: 'sm',
+                            md: 'md',
+                            lg: 'lg',
+                            xl: 'lg',
+                        }
+                    }}>
+                        Settings
+                    </Button>
                 </Animated.View>
                 <Animated.View
-                    style={[{}, style2]}
+                    style={[{ width: '100%' }, style2]}
                 >
-                    <Text>Job applyings</Text>
+                    <Button paddingLeft={5} justifyContent={'flex-start'} borderRadius={'2xl'} variant={'ghost'} mb={2} leftIcon={<Icon as={Feather} size='md' name="inbox" marginRight={4} />} _text={{
+                        fontSize: {
+                            base: 'none',
+                            sm: 'sm',
+                            md: 'md',
+                            lg: 'lg',
+                            xl: 'lg',
+                        }
+                    }}>
+                        Job Applyings
+                    </Button>
                 </Animated.View>
             </View>
-            <View
-                style={{
-                    marginLeft:'auto',
-                    marginRight:'auto',
-                    justifyContent: 'center',
-                    alignItems: 'flex-start',
-                    width: '40%',
-                    flexDirection: 'column',
-                }}>
-                <Heading size="xl">
-                    Profile
-                </Heading>
-
-                <Divider alignSelf="center" thickness="2" orientation="horizontal" />
-                <Heading size="md">
-                    General info
-                </Heading>
-                <FormControl>
-                    <FormControl.Label>Your name</FormControl.Label>
-                    <Input defaultValue='Leon' />
-                </FormControl>
-                <FormControl>
-                    <FormControl.Label>Birth date</FormControl.Label>
-                    <Input defaultValue='15-12-1995' />
-                </FormControl>
-                <FormControl>
-                    <FormControl.Label>Country</FormControl.Label>
-                    <Input defaultValue='The Netherlands' />
-                </FormControl>
-                <FormControl>
-                    <FormControl.Label>City</FormControl.Label>
-                    <Input defaultValue='Utrecht' />
-                </FormControl>
-                <Checkbox value="yes" defaultIsChecked>
-                    European Union citizenship
-                </Checkbox>
-
-                <Divider alignSelf="center" thickness="2" orientation="horizontal" />
-                <Heading size="md">
-                    Work Experience
-                </Heading>
-                <Box my={5} px={10} pb={10} pt={5} rounded="3xl" borderColor="coolGray.200" borderWidth="1" shadow={3} backgroundColor="white">
-                    <Stack justifyContent={'end'} direction={'row'}>
-                        <IconButton variant="ghost" _icon={{
-                            as: Feather,
-                            name: "edit",
-                            color: 'black'
-                        }} />
-                        <IconButton variant="ghost" _icon={{
-                            as: AntDesign,
-                            name: "delete",
-                            color: 'black'
-                        }} />
-                    </Stack>
-                    <Stack mt={5} direction={'row'}>
-                        <Stack direction={'column'}>
-                            <Heading size={'sm'}>Thinkwise</Heading>
-                            <Text>September 2020-Present</Text>
-                            <Text>Apeldoorn, Netherlands</Text>
-                        </Stack>
-                        <Stack ml={10} direction={'column'} width={'40%'}>
-                            <Heading size={'sm'}>Front-end developer</Heading>
-                            <Text>Working with dynamic software make develop of this software very interesting</Text>
-                        </Stack>
-                        <Stack ml={10} direction={'column'} width={'40%'}>
-                            <Heading size={'sm'}>Code skills</Heading>
-                            <Flex mt={2} direction={'row'} flexWrap={"wrap"}>
-                                <Chip title="React" containerStyle={{ marginRight: 5, marginTop: 2 }} />
-                                <Chip title="Redux" containerStyle={{ marginRight: 5, marginTop: 2 }} />
-                                <Chip title="Rxjs" containerStyle={{ marginRight: 5, marginTop: 2 }} />
-                                <Chip title="SQL" containerStyle={{ marginRight: 5, marginTop: 2 }} />
-                                <Chip title="Typescript" containerStyle={{ marginRight: 5, marginTop: 2 }} />
-                                <Chip title="Jest" containerStyle={{ marginRight: 5, marginTop: 2 }} />
-                                <Chip title="Enzyme" containerStyle={{ marginRight: 5, marginTop: 2 }} />
-                            </Flex>
-                        </Stack>
-                    </Stack>
-                </Box>
-                <Divider alignSelf="center" thickness="2" orientation="horizontal" />
-                <Heading size="md">
-                    Links
-                </Heading>
-                <FormControl>
-                    <FormControl.Label>Github</FormControl.Label>
-                    <Input defaultValue='Leon' />
-                </FormControl>
-                <FormControl>
-                    <FormControl.Label>Gitlab</FormControl.Label>
-                    <Input defaultValue='Leon' />
-                </FormControl>
-                <FormControl>
-                    <FormControl.Label>Linkedin</FormControl.Label>
-                    <Input defaultValue='Leon' />
-                </FormControl>
-                <Divider alignSelf="center" thickness="2" orientation="horizontal" />
-                <Heading size="md">
-                    Education
-                </Heading>
-                <Box my={5} px={10} pb={10} pt={5} rounded="3xl" borderColor="coolGray.200" borderWidth="1" shadow={3} backgroundColor="white" width={'100%'}>
-                    <Stack justifyContent={'end'} direction={'row'}>
-                        <IconButton variant="ghost" _icon={{
-                            as: Feather,
-                            name: "edit",
-                            color: 'black'
-                        }} />
-                        <IconButton variant="ghost" _icon={{
-                            as: AntDesign,
-                            name: "delete",
-                            color: 'black'
-                        }} />
-                    </Stack>
-                    <Stack direction={'column'}>
-                        <Heading size={'sm'}>Avans Hogeschool</Heading>
-                        <Text>September 2018-Present</Text>
-                        <Text>Bachelor of science IT</Text>
-                    </Stack>
-                </Box>
-                <Divider alignSelf="center" thickness="2" orientation="horizontal" />
-                <Heading size="md">
-                    About
-                </Heading>
-                <Text>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </Text>
-            </View>
+            <UserProfile />
         </View>
     );
 }
---
+++ src/Components/UserOptions/UserProfile.tsx
@@ -0,0 +1,170 @@
+import { AntDesign, Feather } from '@expo/vector-icons';
+import { Chip } from '@rneui/themed';
+import { Box, Checkbox, Divider, Flex, FormControl, Heading, IconButton, Input, Stack, Text } from 'native-base';
+import React from 'react';
+import { Dimensions } from 'react-native';
+
+export default function UserProfile() {
+    return (
+        <Box
+            w={{
+                base: '90%',
+                sm: '90%',
+                md: '60%',
+                lg: '50%',
+                xl: '45%',
+            }}
+            style={{
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                justifyContent: 'center',
+                alignItems: 'flex-start',
+                flexDirection: 'column',
+                marginBottom: 24
+            }}>
+            <Heading size="xl">
+                Profile
+            </Heading>
+
+            <Divider alignSelf="center" thickness="2" orientation="horizontal" />
+            <Heading size="md">
+                General info
+            </Heading>
+            <FormControl>
+                <FormControl.Label>Your name</FormControl.Label>
+                <Input defaultValue='Leon' />
+            </FormControl>
+            <FormControl>
+                <FormControl.Label>Birth date</FormControl.Label>
+                <Input defaultValue='15-12-1995' />
+            </FormControl>
+            <FormControl>
+                <FormControl.Label>Country</FormControl.Label>
+                <Input defaultValue='The Netherlands' />
+            </FormControl>
+            <FormControl>
+                <FormControl.Label>City</FormControl.Label>
+                <Input defaultValue='Utrecht' />
+            </FormControl>
+            <Checkbox value="yes" defaultIsChecked>
+                European Union citizenship
+            </Checkbox>
+
+            <Divider alignSelf="center" thickness="2" orientation="horizontal" />
+            <Heading size="md">
+                Work Experience
+            </Heading>
+            <Box my={5} px={6} pb={6} pt={2} rounded="3xl" borderColor="coolGray.200" borderWidth="1" shadow={3}>
+                <Stack justifyContent={'end'} direction={'row'}>
+                    <IconButton variant="ghost" _icon={{
+                        as: Feather,
+                        name: "edit",
+                        color: 'black'
+                    }} />
+                    <IconButton onPress={() => alert(Dimensions.get("window").width)} variant="ghost" _icon={{
+                        as: AntDesign,
+                        name: "delete",
+                        color: 'black'
+                    }} />
+                </Stack>
+                <Stack mt={5} direction={{ base: 'column', sm: 'column', md: 'row' }}>
+                    <Stack direction={'column'} mb={{ base: 2, sm: 2, md: 'none' }}>
+                        <Heading size={'sm'}>Thinkwise</Heading>
+                        <Text>September 2020-Present</Text>
+                        <Text>Apeldoorn, Netherlands</Text>
+                    </Stack>
+                    <Stack ml={{
+                        md: 5,
+                        lg: 6,
+                        xl: 7,
+                    }}
+                        direction={'column'}
+                        w={{
+                            md: '30%',
+                            lg: '35%',
+                            xl: '40%',
+                        }}
+                        mb={{ base: 2, sm: 2, md: 'none' }}>
+                        <Heading size={'sm'}>Front-end developer</Heading>
+                        <Text>Working with dynamic software make develop of this software very interesting</Text>
+                    </Stack>
+                    <Stack
+                        ml={{
+                            md: 5,
+                            lg: 6,
+                            xl: 7,
+                        }}
+                        direction={'column'}
+                        w={{
+                            md: '30%',
+                            lg: '35%',
+                            xl: '40%',
+                        }}>
+                        <Heading size={'sm'}>Code skills</Heading>
+                        <Flex mt={2} mr={{ lg: 0, xl: 7 }} direction={'row'} flexWrap={"wrap"}>
+                            <Chip title="React" containerStyle={{ marginRight: 2, marginTop: 2 }} />
+                            <Chip title="Redux" containerStyle={{ marginRight: 2, marginTop: 2 }} />
+                            <Chip title="Rxjs" containerStyle={{ marginRight: 2, marginTop: 2 }} />
+                            <Chip title="SQL" containerStyle={{ marginRight: 2, marginTop: 2 }} />
+                            <Chip title="Typescript" containerStyle={{ marginRight: 2, marginTop: 2 }} />
+                            <Chip title="Jest" containerStyle={{ marginRight: 2, marginTop: 2 }} />
+                            <Chip title="Angular" containerStyle={{ marginRight: 2, marginTop: 2 }} />
+                            <Chip title="Laravel" containerStyle={{ marginRight: 2, marginTop: 2 }} />
+                            <Chip title="Phyton" containerStyle={{ marginRight: 2, marginTop: 2 }} />
+                        </Flex>
+                    </Stack>
+                </Stack>
+            </Box>
+
+            <Divider alignSelf="center" thickness="2" orientation="horizontal" />
+            <Heading size="md">
+                Links
+            </Heading>
+            <FormControl>
+                <FormControl.Label>Github</FormControl.Label>
+                <Input defaultValue='Leon' />
+            </FormControl>
+            <FormControl>
+                <FormControl.Label>Gitlab</FormControl.Label>
+                <Input defaultValue='Leon' />
+            </FormControl>
+            <FormControl>
+                <FormControl.Label>Linkedin</FormControl.Label>
+                <Input defaultValue='Leon' />
+            </FormControl>
+
+            <Divider alignSelf="center" thickness="2" orientation="horizontal" />
+            <Heading size="md">
+                Education
+            </Heading>
+            <Box my={5} px={6} pb={6} pt={2} rounded="3xl" borderColor="coolGray.200" borderWidth="1" shadow={3} backgroundColor="white" width={'100%'}>
+                <Stack justifyContent={'end'} direction={'row'}>
+                    <IconButton variant="ghost" _icon={{
+                        as: Feather,
+                        name: "edit",
+                        color: 'black'
+                    }} />
+                    <IconButton variant="ghost" _icon={{
+                        as: AntDesign,
+                        name: "delete",
+                        color: 'black'
+                    }} />
+                </Stack>
+                <Stack direction={'column'}>
+                    <Heading size={'sm'}>Avans Hogeschool</Heading>
+                    <Text>September 2018-Present</Text>
+                    <Text>Bachelor of science IT</Text>
+                </Stack>
+            </Box>
+            <Divider alignSelf="center" thickness="2" orientation="horizontal" />
+            <Heading size="md">
+                About
+            </Heading>
+            <Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Text>
+        </Box>
+    )
+}
