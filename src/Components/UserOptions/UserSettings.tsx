67dc2dd471b9c48bf242acbb34c315fe26ce4598
LJstudent <poepjan123_2@hotmail.com>
2023-03-25 03:38:11 +0100

    New version react native and beginning with user settings

--- src/Components/UserOptions/UserSettings.tsx
+++ src/Components/UserOptions/UserSettings.tsx
@@ -1,10 +1,129 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import { Button, ButtonGroup } from '@rneui/themed';
+import { Box, Heading, Stack, Text, Icon } from 'native-base';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { FontAwesome, MaterialIcons } from '@expo/vector-icons';
+
+export enum JobStatus {
+    Denied = 'Denied',
+    Pending = 'Pending',
+    Approved = 'Approved'
+}
+
+function GetJobStatus({ status }: any) {
+
+    if (status === JobStatus.Denied) {
+        return <Icon as={MaterialIcons} name="highlight-off" size={'lg'} color={"danger.800"} />
+    }
+
+    if (status === JobStatus.Approved) {
+        return <Icon as={MaterialIcons} name="check-circle-outline" size={'lg'} color={"tertiary.500"} />
+    }
+
+    return <Icon as={MaterialIcons} name="hourglass-empty" size={'lg'} color={"violet.500"} />;
+}
 
 export default function UserSettings() {
+    const [disableBtn] = useState(true);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    //test value
+    const jbStsTest = 'Approved';
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Settings!</Text>
-        </View>
+        <Box flex={1} alignItems={'center'} mt={10}>
+            <Heading size="md" mb={2}>Filter</Heading>
+            <ButtonGroup
+                buttons={['Denied', 'Pending', 'Approved']}
+                selectedIndex={selectedIndex}
+                onPress={(value) => {
+                    setSelectedIndex(value);
+                }}
+                buttonStyle={{
+                    backgroundColor: 'white',
+                    borderColor: '#8b5cf6',
+                    borderWidth: 1,
+                    borderRadius: 20,
+                    width: 100,
+                    marginHorizontal: 4,
+
+                }}
+                containerStyle={{
+                    borderWidth: undefined,
+                    borderEndWidth: undefined,
+
+                }}
+                textStyle={{ fontSize: 14, color: '#8b5cf6' }}
+                innerBorderStyle={{ width: 0 }}
+                selectedButtonStyle={{ backgroundColor: '#8b5cf6' }}
+            />
+            <Box mt={4} rounded={'xl'} shadow={9} backgroundColor="white" w={{ sm: '80%', md: '400px' }} maxWidth={{ sm: '400px', md: '400px' }}>
+                <Stack direction={'row'} m={4}>
+                    <Box w={'100px'} height={'100px'} shadow={3} rounded='xl' backgroundColor={'white'}>
+                    </Box>
+                    <Stack ml={4} direction={'column'}>
+                        <Heading>
+                            Mars
+                        </Heading>
+                        <Text mt={1} fontSize="lg" _light={{
+                            color: "violet.500"
+                        }} fontWeight="500">
+                            Front-end developer
+                        </Text>
+                    </Stack>
+                </Stack>
+                <Box ml={4} my={1}>
+                    <Text fontWeight={500}>Date of applying:</Text>
+                    <Text mt={1}>25-03-2023</Text>
+                </Box>
+                <Box ml={4} my={1}>
+                    <Text fontWeight={500}>Status of your job apply:</Text>
+                    <GetJobStatus
+                        status={jbStsTest}
+                    />
+                </Box>
+                <Box justifyContent='center' alignItems='center'>
+                    <Stack direction={'row'} h={16} w={275} rounded="xl" shadow={3} backgroundColor="#FCF7FE">
+                        <Text m={4} fontWeight={'bold'} alignSelf='center'>Check the vacancy</Text>
+                        <Button
+                            title="Open"
+                            buttonStyle={{
+                                backgroundColor: '#8b5cf6',
+                                borderWidth: 1,
+                                borderColor: 'white',
+                                borderRadius: 10,
+                            }}
+                            containerStyle={{
+                                width: 100,
+                                alignSelf: 'center'
+                            }}
+                            titleStyle={{ fontSize: 14 }}
+                        />
+                    </Stack>
+                    <Stack m={4}>
+                        {jbStsTest !== JobStatus.Denied &&
+                            <Button
+                                disabled={disableBtn}
+                                buttonStyle={{
+                                    backgroundColor: '#8b5cf6',
+                                    borderWidth: 2,
+                                    borderColor: 'white',
+                                    borderRadius: 20,
+                                }}
+                                containerStyle={{
+                                    width: 300,
+                                }}
+                                titleStyle={{ fontSize: 14 }} >
+                                {disableBtn ? 'Waiting for response...' : 'They are interesed!'}
+                                {disableBtn ?
+                                    <Icon as={FontAwesome} ml={2} size={5} name="lock" color="muted.400" />
+                                    :
+                                    <Icon as={FontAwesome} ml={2} size={5} name="unlock" color="white" />
+                                }
+                            </Button>
+                        }
+                    </Stack>
+                </Box>
+            </Box>
+        </Box>
     );
 }
